e39eeb307a4f7aab83fbaf52180a22f22c08c029
James Kermode <james.kermode@gmail.com>
2025-11-01 19:23:28 +0000

    Fix build system: switch from meson to setuptools with f2py
    - Replace meson-python with setuptools in pyproject.toml
    - Update setup.py to use custom build_ext with f2py
    - Remove meson.build (incompatible with f2py workflow)
    - This properly generates Python extension modules using numpy.f2py

--- setup.py
+++ setup.py
@@ -1,27 +1,63 @@
 #!/usr/bin/env python
+"""Setup script for py-design with NumPy 2.x compatibility"""
 
-
-from numpy.distutils.core import setup
-from numpy.distutils.core import Extension
 import os
-import platform
 import glob
+import sys
+import subprocess
+from pathlib import Path
+from setuptools import setup
+from setuptools.command.build_ext import build_ext as _build_ext
+import numpy as np
 
 
-extra_link_args = []
-if platform.system == 'Darwin':
-    extra_link_args = ['-undefined dynamic_lookup']
+class f2py_build_ext(_build_ext):
+    """Custom build_ext command that uses f2py to compile Fortran"""
 
-setup(name='py-design',
-      version='2.0',
-      description='Design points for random experiments',
-      author='Ilias Bilionis',
-      author_email='ibilion@purdue.edu',
-      url='https://github.com/ebilionis/py-design',
-      download_url='https://github.com/ebilionis/py-design/tarball/1.0',
-      keywards=['design', 'random experiment', 'latin hypercube',
-                'sparse grid', 'computer experiments'],
-      ext_modules=[Extension('design._design',
-                            glob.glob(os.path.join('src', '*.f90')),
-                            extra_link_args=extra_link_args)],
-      packages=['design'])
+    def build_extension(self, ext):
+        # Get source files
+        sources = [str(Path(s).resolve()) for s in ext.sources]
+
+        # Output directory
+        build_dir = Path(self.build_lib) / 'design'
+        build_dir.mkdir(parents=True, exist_ok=True)
+
+        # Module name
+        module_name = ext.name.split('.')[-1]
+
+        # Call f2py
+        f2py_cmd = [
+            sys.executable, '-m', 'numpy.f2py',
+            '-c',
+            '--build-dir', str(self.build_temp),
+        ] + sources + [
+            '-m', module_name,
+        ]
+
+        print(f"Running f2py: {' '.join(f2py_cmd)}")
+        result = subprocess.run(f2py_cmd, cwd=str(Path.cwd()))
+
+        if result.returncode != 0:
+            raise RuntimeError("f2py compilation failed")
+
+        # Move the compiled module to the right place
+        import shutil
+        for item in Path('.').glob(f'{module_name}.*.so'):
+            shutil.move(str(item), str(build_dir / item.name))
+
+
+# Get all Fortran source files
+fortran_sources = glob.glob(os.path.join('src', '*.f90'))
+
+if __name__ == "__main__":
+    from setuptools import Extension
+
+    ext_module = Extension(
+        name='design._design',
+        sources=fortran_sources,
+    )
+
+    setup(
+        ext_modules=[ext_module],
+        cmdclass={'build_ext': f2py_build_ext},
+    )
